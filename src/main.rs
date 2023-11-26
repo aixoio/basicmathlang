1141e45902cf75bfa821e6f3b0bb58f00813846d
aixoio <79969530+aixoio@users.noreply.github.com>
2023-11-26 20:00:08 +1100

    Add args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::env;
+
 use basicmathlang::{reader, tokens, parser, runner};
 
 fn main() {
-    let file_data = reader::read_file("math.bml").unwrap();
+    let args: Vec<String> = env::args().collect();
+    let file_data = reader::read_file(args.get(1).unwrap()).unwrap();
     let direct_tokens = tokens::parse_to_tokens(file_data);
     let parsered_tokens = parser::parse_tokens(&direct_tokens);
 

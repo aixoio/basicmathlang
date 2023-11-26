9f123be427243cd19880d5aecccc429443eb1ffd
aixoio <79969530+aixoio@users.noreply.github.com>
2023-11-26 20:01:21 +1100

    Bug fix

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use std::env;
+use std::{env, process};
 
 use basicmathlang::{reader, tokens, parser, runner};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: <filename>");
+        process::exit(1);
+    }
+
     let file_data = reader::read_file(args.get(1).unwrap()).unwrap();
     let direct_tokens = tokens::parse_to_tokens(file_data);
     let parsered_tokens = parser::parse_tokens(&direct_tokens);
